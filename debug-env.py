c6b07ef2782c0e9fdd5898941eff558c26b6ff42
cheerfulinsanity <cheerfulinsanity@gmail.com>
2025-07-30 12:15:13 +1200

    Delete debug-env.py

--- debug-env.py
+++
@@ -1,6 +0,0 @@
-import os
-
-print("🔍 Dumping environment variables containing 'GIST' or 'TOKEN'...\n")
-for key, value in os.environ.items():
-    if "GIST" in key or "TOKEN" in key:
-        print(f"{key} = {value[:6]}... (truncated)")
