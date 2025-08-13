8aae75445a0df7d98d62ad492321b4808b063bb6
cheerfulinsanity <cheerfulinsanity@gmail.com>
2025-08-14 03:07:43 +1200

    Update __init__.py

--- bot/formatter_pkg/__init__.py
+++ bot/formatter_pkg/__init__.py
@@ -1,5 +1,6 @@
 # bot/formatter_pkg/__init__.py
 from .core import format_match_embed
 from .embed_builder import build_discord_embed
+from .helpers import NORMAL_STATS, TURBO_STATS  # optionally re-export here, too
 
-__all__ = ["format_match_embed", "build_discord_embed"]
+__all__ = ["format_match_embed", "build_discord_embed", "NORMAL_STATS", "TURBO_STATS"]
